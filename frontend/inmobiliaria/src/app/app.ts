eed6a47812da1395c3f5b4cc110daa469cc10a8d
blicsi <pablorubengross@gmail.com>
2026-04-10 21:54:59 -0300

    adicion de material ui

--- frontend/inmobiliaria/src/app/app.ts
+++ frontend/inmobiliaria/src/app/app.ts
@@ -4,10 +4,12 @@ import { Listing } from './component/listing/listing';
 import { Menu } from './component/menu/menu';
 import { Footer } from './component/footer/footer';
 import { Busqueda } from './component/busqueda/busqueda';
+import { MatDivider } from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Listing,Menu,Footer,Busqueda],
+  imports: [RouterOutlet,Listing,Menu,Footer,Busqueda,MatDivider],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- frontend/inmobiliaria/src/app/component/menu/menu.ts
+++ frontend/inmobiliaria/src/app/component/menu/menu.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButton, MatFabButton} from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { Login } from '../login/login';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [MatButton, MatFabButton,MatIcon],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
